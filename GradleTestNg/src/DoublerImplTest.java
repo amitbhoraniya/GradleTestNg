b29fee597cf76133c8ba3338e6d1867565de83fc
amitbhoraniya <amitbhoraniya007@gmail.com>
2015-03-09 16:11:35 +0530

    First Commit

---
+++ GradleTestNg/src/DoublerImplTest.java
@@ -0,0 +1,14 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class DoublerImplTest {
+    
+    @Test
+    public void testIt() {
+        
+    	System.out.println("HElloo World");
+    	Reporter.log("Hellooooo");
+        assert 4 == 4;
+    }
+}
